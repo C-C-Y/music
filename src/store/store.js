c6e0a6de05890055bb59f07166b0e264f261921e
kim Chao <2500347626@qq.com>
2019-03-16 17:30:47 +0800

    取消生产环境日志

--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,15 @@ import state from "./state";
 import mutations from "./mutations";
 import actions from "./actions";
 import * as getters from "./getters";
-import createLogger from "vuex/dist/logger";
+// import createLogger from "vuex/dist/logger";
 
 Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== "production";
+/* const debug = process.env.NODE_ENV !== "production"; */
 export default new Vuex.Store({
   state,
   mutations,
   actions,
-  getters,
-  plugins: debug ? [createLogger()] : []
+  getters
+  // plugins: debug ? [createLogger()] : []
 });
